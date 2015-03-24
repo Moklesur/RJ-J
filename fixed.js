db76f67579fc84d9ed66f1ba26f8770957f9aa48
Md. Moklesur Rahman <moklesur42@gmail.com>
2015-03-24 22:47:56 +0600

    sp-carousel

--- fixed.js
+++ fixed.js
@@ -30,6 +30,7 @@
                         $(this).children().removeClass('hover');
                     }
                 },
+
                 auto: true
             });
             $('#Container').mixItUp();
